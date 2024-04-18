aea4127828159fd245eb0432003fbeb21381faaa
chinmay-joshi_cytiva <chinmay-joshi_cytiva>
2024-04-18 15:21:22 +0200

    Test

--- game.py
+++ game.py
@@ -3,10 +3,10 @@ import time
 from sys import exit
 pygame.init()
 
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
 SPEED = 5
-FLOOR_Y = 625
+FLOOR_Y = 665
 
 RED   = (255, 0, 0)
 
@@ -14,21 +14,22 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("GAME")
 clock = pygame.time.Clock()
 
-test_sky = pygame.image.load("images/sky.jpeg")
-test_ground = pygame.image.load("images/ground.png")
-test_cactus = pygame.image.load("images/cactus.tiff")
+sky = pygame.image.load("images/sky.jpeg").convert()
+ground = pygame.image.load("images/ground.png").convert()
+# cactus = pygame.image.load("images/cactus.tiff").convert()
 
 class Enemy(pygame.sprite.Sprite):
-      def __init__(self):
+      def __init__(self,path,x,y):
         super().__init__() 
-        self.image = pygame.image.load("images/cactus.tiff")
+        self.image = pygame.image.load(path).convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.center = (900, FLOOR_Y)    
+        self.rect.center = (x, y)
+        self.x = x
  
       def move(self):
         self.rect.move_ip(-SPEED,0)
         if (self.rect.right < 0):
-            self.rect.center = (1210, FLOOR_Y)
+            self.rect.center = (SCREEN_WIDTH, self.rect.center[1])
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -55,30 +56,27 @@ class Player(pygame.sprite.Sprite):
 
 #Setting up Sprites        
 P1 = Player()
-E1 = Enemy()
+E1 = Enemy("images/cactus.tiff",1300,665)
+E2 = Enemy("images/vulture.png",1500,200)
  
 #Creating Sprites Groups
 enemies = pygame.sprite.Group()
 enemies.add(E1)
+enemies.add(E2)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(E1)
-
-#Adding a new User event 
-INC_SPEED = pygame.USEREVENT + 1
-pygame.time.set_timer(INC_SPEED, 1000)
+all_sprites.add(E2)
 
 while True:
     for event in pygame.event.get():
-        if event.type == INC_SPEED:
-            SPEED += 2
 
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    screen.blit(test_sky,(0,0)) 
-    screen.blit(test_ground,(0,700))
+    screen.blit(sky,(0,0)) 
+    screen.blit(ground,(0,700))
     
     #Moves and Re-draws all Sprites
     for entity in all_sprites:

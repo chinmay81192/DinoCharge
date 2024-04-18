97523d660255f17809f40ab9b91a2fe288c53ed6
João Romero <joao.romero@cytiva.com>
2024-04-18 13:37:51 +0200

    Added main mechanics

--- game.py
+++ game.py
@@ -1,7 +1,16 @@
 import pygame
+import time
 from sys import exit
 pygame.init()
-screen = pygame.display.set_mode((1200,800))
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+SPEED = 5
+FLOOR_Y = 625
+
+RED   = (255, 0, 0)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("GAME")
 clock = pygame.time.Clock()
 
@@ -9,17 +18,82 @@ test_sky = pygame.image.load("images/sky.jpeg")
 test_ground = pygame.image.load("images/ground.png")
 test_cactus = pygame.image.load("images/cactus.tiff")
 
-cactus_x_position = 900
+class Enemy(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("images/cactus.tiff")
+        self.rect = self.image.get_rect()
+        self.rect.center = (900, FLOOR_Y)    
+ 
+      def move(self):
+        self.rect.move_ip(-SPEED,0)
+        if (self.rect.right < 0):
+            self.rect.center = (1210, FLOOR_Y)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("images/Player.jpg")
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.rect.width / 2, FLOOR_Y)
+        
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if self.rect.top > 0:
+            if pressed_keys[pygame.K_UP]:
+                self.rect.move_ip(0, -5)
+        if self.rect.bottom < SCREEN_HEIGHT:
+            if pressed_keys[pygame.K_DOWN]:
+                self.rect.move_ip(0,5)
+         
+        #if self.rect.left > 0:
+        #      if pressed_keys[K_LEFT]:
+        #          self.rect.move_ip(-5, 0)
+        #if self.rect.right < SCREEN_WIDTH:        
+        #      if pressed_keys[K_RIGHT]:
+        #          self.rect.move_ip(5, 0)
+
+#Setting up Sprites        
+P1 = Player()
+E1 = Enemy()
+ 
+#Creating Sprites Groups
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+
+#Adding a new User event 
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
 while True:
     for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED += 2
+
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
     screen.blit(test_sky,(0,0)) 
     screen.blit(test_ground,(0,700))
-    if cactus_x_position < -10:
-        cactus_x_position = 1210
-    cactus_x_position -= 4
-    screen.blit(test_cactus,(cactus_x_position,625))      
+    
+    #Moves and Re-draws all Sprites
+    for entity in all_sprites:
+        screen.blit(entity.image, entity.rect)
+        entity.move()
+ 
+    #To be run if collision occurs between Player and Enemy
+    if pygame.sprite.spritecollideany(P1, enemies):
+          screen.fill(RED)
+          pygame.display.update()
+          for entity in all_sprites:
+                entity.kill() 
+          time.sleep(2)
+          pygame.quit()
+          exit()
+
     pygame.display.update()
     clock.tick(60)
